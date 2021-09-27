561b402cfe7d3d99a68013dbdff72117548ee2d0
jamila <kadurbekova03@gmail.com>
2021-09-27 21:40:18 +0600

    secondProject

---
+++ css/js/main.js
@@ -0,0 +1,34 @@
+// alert('sdlk)
+// 'hello' - "H"
+// Not a Namber
+
+// console.log("sasdasd" + "5" + 122345)
+
+// Get Number
+//  x = prompt("Введите номер", +996)
+
+// display number  
+// console.log('Ваш номерЖ + х') +99600004444
+
+// Ваш номер: +9960333333
+
+x = prompt("X")
+y = prompt("Y")
+
+if(x > y) {
+     console.log("X more then Y")
+    console.log("X = " + x)
+    console.log("Y =" + y)
+} else if(x > 4){
+    console.log("X less then Y")
+}
+
+
+z = prompt("Z")
+w = prompt("W")
+
+
+if(z > w){
+    console.log()
+}
+
